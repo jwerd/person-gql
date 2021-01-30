33a797907352543c51b4bf310d641836609401f8
Jake Vivian <jacob@sadsoft.com>
2021-01-30 13:51:26 -0800

    remove passport, let's use sanctum instead.

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,14 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('user', function () {
+    $user = \App\Models\User::updateOrCreate(['email' => 'test@example.com'],
+    [
+        'name' => 'Sample User',
+        'email' => 'test@example.com',
+        'password' => bcrypt('secret')
+    ]);
+    // $token = $user->createToken('GraphQL Accesss')->accessToken;
+    // dump($token);
+})->describe('Create sample user');
